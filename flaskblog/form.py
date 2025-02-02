96beb2d4e96fd7a9aaae710bffc421f14915a3a3
Arvind <arvindsingh.2021cs@technonjr>
2025-02-02 23:49:21 +0530

    User account and profile,CRUD operation,Pagination Done

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,5 +1,8 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from flask_wtf.file import FileField, file_allowed
+from wtforms import (BooleanField, PasswordField, StringField, SubmitField,
+                     TextAreaField)
 from wtforms.validators import (Email, EqualTo, ValidationError, data_required,
                                 length)
 
@@ -27,3 +30,25 @@ class LoginForm(FlaskForm):
     password=PasswordField('password',validators=[data_required(),])
     remember=BooleanField('Remember me')
     submit=SubmitField('Login')
+    
+class UpdateAccountForm(FlaskForm):
+    username=StringField('username',validators=[data_required(),length(min=2,max=20)])
+    email=StringField('email',validators=[data_required(),Email()])
+    picture=FileField('Update picture',validators=[file_allowed(['jpg', 'png'])])
+    submit=SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data!=current_user.username:
+            user=User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken.Please choose a different one.')
+    def validate_email(self, email):
+        if email.data!=current_user.email:
+            user=User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken.Please choose a different one.')
+            
+class PostForm(FlaskForm):
+    title=StringField('Title',validators=[data_required()])
+    content=TextAreaField('Content',validators=[data_required()])
+    submit=SubmitField('Post')
